f3d11f80c76b8d0e3c027419e51830e0f42f0172
samwinp <samwinpereira@gmail.com>
2024-07-18 15:26:46 +0530

    added states

--- submition1/main.py
+++ submition1/main.py
@@ -6,6 +6,12 @@ import requests
 
 app = FastAPI()
 
+windowPrevState = []
+windowCurrentState = []
+numbers = []
+avg = 0
+
+
 token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzIxMjk2NDcwLCJpYXQiOjE3MjEyOTYxNzAsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImY3NmQxMjZlLTNmMTEtNDBjYi1hMDFmLTEyNzNmYzQ3NGE3NCIsInN1YiI6IjIxZDUxLnNhbXdpbkBzamVjLmFjLmluIn0sImNvbXBhbnlOYW1lIjoiZ29NYXJ0IiwiY2xpZW50SUQiOiJmNzZkMTI2ZS0zZjExLTQwY2ItYTAxZi0xMjczZmM0NzRhNzQiLCJjbGllbnRTZWNyZXQiOiJtandaTVdxUW9rZFZBTnpzIiwib3duZXJOYW1lIjoic2Ftd2luIiwib3duZXJFbWFpbCI6IjIxZDUxLnNhbXdpbkBzamVjLmFjLmluIiwicm9sbE5vIjoiNFNPMjFDUzE0MCJ9.aPkHZ8AmBZvSxqza7EctUi8HQH7as3pnBoSkRNh-YJA"
 @app.get("/")
 def say_hi():
